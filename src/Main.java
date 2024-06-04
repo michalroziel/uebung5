4e14553db2edea68172be18f8b8d69337f6013f2
Michał Roziel <rozielmichal@gmail.com>
2024-06-04 11:51:11 +0200

    started faculty

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,41 @@ public class Main {
         MyFunction printFun = (int i) -> i ;
 
         myDriver.applyAndPrint(printFun, 1, 5);
+
+
+        System.out.println("************ exercise b ************");
+
+        System.out.println( "exercise 5b1 using lambda expression:");
+
+        MyFunction squareFun = (int i) -> i * i;
+        // TODO: is this correct? as we have x and y
+        myDriver.applyAndPrint(squareFun, 2, 5);
+
+
+        System.out.println("exercise 5b1 using anonymous class:");
+        MyFunction obj = new MyFunction()
+        {
+            public int apply(int i)
+            {
+                return i * i;
+            }
+        };
+
+
+        System.out.println("************ ******* ************");
+
+        System.out.println( "exercise 5b2 using lambda expression:");
+
+        MyFunction faculty = (int i) -> {
+            int result = 1;
+            for (int j = i; j > 0; j--) {
+                result *= j;
+            }
+            return result;
+        };
+
+
+
     }
 
 
